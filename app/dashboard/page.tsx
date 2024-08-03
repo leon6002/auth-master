1d490f6eefc1f7d9879ec641d265c645c4e0dfd7
gulong <gulong.chen@hotmail.com>
2024-08-03 20:53:22 +0800

    submit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth";
 import Dashboard from "@/components/Dashboard";
+
 import { db } from "@/lib/db";
 // import { getUserSubscriptionPlan } from "@/lib/stripe";
 // import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
@@ -8,22 +9,22 @@ import { redirect } from "next/navigation";
 const Page = async () => {
   // const { getUser } = getKindeServerSession();
   const session = await auth();
-  if (!session?.user) return null
+  if (!session?.user) return null;
 
-  const user = session.user
+  const user = session.user;
 
   const dbUser = await db.user.findFirst({
     where: {
       id: user.id,
-    }
+    },
   });
 
   if (!dbUser) redirect("/auth-callback?origin=dashboard");
 
   // const subscriptionPlan = await getUserSubscriptionPlan();
   const subscriptionPlan = {
-    isSubscribed: false
-  }
+    isSubscribed: false,
+  };
 
   return <Dashboard subscriptionPlan={subscriptionPlan} />;
 };
