31dbf3d54db1e36fdf2f9ff89336e1953ef4f591
gulong <gulong.chen@hotmail.com>
2024-10-22 13:29:15 +0800

    final commit

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,11 +4,12 @@ import * as React from "react";
 
 import { useSidebar } from "@/lib/hooks/use-sidebar";
 import { cn } from "@/lib/utils";
+import { ArrowLeftCircle, ArrowRightCircle } from "lucide-react";
 
 export interface SidebarProps extends React.ComponentProps<"div"> {}
 
 export function Sidebar({ className, children }: SidebarProps) {
-  const { isSidebarOpen, isLoading } = useSidebar();
+  const { isSidebarOpen, isLoading, toggleSidebar } = useSidebar();
 
   return (
     <div
@@ -18,6 +19,18 @@ export function Sidebar({ className, children }: SidebarProps) {
         "grainy h-full flex-col dark:bg-zinc-950 dark:bg-none",
       )}
     >
+      <div className="absolute h-screen w-full">
+        <div
+          className="absolute -right-8 top-1/2 cursor-pointer text-muted-foreground"
+          onClick={toggleSidebar}
+        >
+          {isSidebarOpen ? (
+            <ArrowLeftCircle size={25} />
+          ) : (
+            <ArrowRightCircle size={25} />
+          )}
+        </div>
+      </div>
       {children}
     </div>
   );
